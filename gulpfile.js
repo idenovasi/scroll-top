7abfcf0e480b5ff6faad333ee4b1e50e15e4e196
Satrya <margasatrya@gmail.com>
2021-02-09 18:06:56 +0700

    Add `.github` dir

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,61 @@
-"use strict";
+'use strict';
 
-const { series, watch, src, dest, parallel } = require("gulp");
+const { series, watch, src, dest, parallel } = require('gulp');
 
 // Gulp plugins & utilities
-const del = require("del");
-const zip = require("gulp-zip");
-const wpPot = require("gulp-wp-pot");
+const del = require('del');
+const zip = require('gulp-zip');
+const wpPot = require('gulp-wp-pot');
 
 /**
  * Copy files for production
  */
 function copyFiles() {
   return src([
-    "**",
-    "!*.map",
-    "!node_modules/**",
-    "!dist/**",
-    "!sass/**",
-    "!.git/**",
-    "!gulpfile.js",
-    "!package.json",
-    "!package-lock.json",
-    "!.editorconfig",
-    "!.gitignore",
-    "!.jshintrc",
-    "!.DS_Store",
-    "!*.map",
-  ]).pipe(dest("dist/scroll-top/"));
+    '**',
+    '!*.map',
+    '!node_modules/**',
+    '!dist/**',
+    '!sass/**',
+    '!.git/**',
+    '!.github/**',
+    '!gulpfile.js',
+    '!package.json',
+    '!package-lock.json',
+    '!.editorconfig',
+    '!.gitignore',
+    '!.jshintrc',
+    '!.DS_Store',
+    '!*.map',
+  ]).pipe(dest('dist/scroll-top/'));
 }
 
 /**
  * Clean folder
  */
 function clean() {
-  return del(["dist/**", "dist"], { force: false });
+  return del(['dist/**', 'dist'], { force: false });
 }
 
 /**
  * Zip folder
  */
 function zipped() {
-  return src(["dist/**"]).pipe(zip("scroll-top.zip")).pipe(dest("dist/"));
+  return src(['dist/**']).pipe(zip('scroll-top.zip')).pipe(dest('dist/'));
 }
 
 /**
  * Generate .pot file
  */
 function language() {
-  return src(["**/*.php", "!dist/**/*"])
+  return src(['**/*.php', '!dist/**/*'])
     .pipe(
       wpPot({
-        domain: "scroll-top",
-        package: "Scroll Top",
+        domain: 'scroll-top',
+        package: 'Scroll Top',
       })
     )
-    .pipe(dest("languages/scroll-top.pot"));
+    .pipe(dest('languages/scroll-top.pot'));
 }
 
 /**
